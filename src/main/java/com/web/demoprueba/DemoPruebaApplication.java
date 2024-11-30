d3e954854d0be6e5289b92b3c58fe2b4a853cd3b
Alberto <alberto.limas.contractor@bbva.com>
2024-11-29 20:20:28 -0500

    fe

--- src/main/java/com/web/demoprueba/DemoPruebaApplication.java
+++ src/main/java/com/web/demoprueba/DemoPruebaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoPruebaApplication {
 
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         SpringApplication.run(DemoPruebaApplication.class, args);
     }
 
